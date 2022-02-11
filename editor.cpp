bbacd0e9551820816b80c34c3051f8d307a1b15f
Michael Rimboim <mrimboim@gmail.com>
2022-02-10 19:08:39 -0800

    command line validation done

--- editor.cpp
+++ editor.cpp
@@ -7,11 +7,21 @@
 
 using namespace std;
 
-#define MAX_WIDTH 20  // Maximum size of a line
-#define  MAX_LINES 30  // Maximum number of lines 
+#define MAX_WIDTH 20 // Maximum size of a line
+#define MAX_LINES 30 // Maximum number of lines
 
 int main(int argc, char *argv[])
 {
-   
+    //intial command line validation 
+    if (argc < 2)
+    {
+        cout << "Too few command-line arguments." << '\n';
+        
+    }else if(argc > 2){
+        cout << "Too many command-line arguments." << '\n';
+    }
+
+    
+
 
 }
